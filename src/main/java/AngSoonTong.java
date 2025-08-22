d4f5e4d25cb699a5abb8f28cd1aa1db01e7fdd6e
lanjia0 <nicholas.chin.wai.kit@gmail.com>
2025-08-22 15:15:10 +0800

    Greeting

---
+++ src/main/java/AngSoonTong.java
@@ -0,0 +1,6 @@
+public class AngSoonTong {
+    public static void main(String[] args) {
+        System.out.println("Eh! I'm Soon Tong\nWhat you want?!");
+        System.out.println("Bye. Why you still here?!");
+    }
+}
