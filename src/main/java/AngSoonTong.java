72389d5db51dd563201e43e423698bb4fc170795
lanjia0 <nicholas.chin.wai.kit@gmail.com>
2025-08-23 18:00:53 +0800

    chatbot now logs todo, event, deadline

--- src/main/java/AngSoonTong.java
+++ src/main/java/AngSoonTong.java
@@ -16,9 +16,36 @@ public class AngSoonTong {
             String curr = sc.nextLine();
             String[] words = curr.split(" "); // split by spaces
             String firstWord = words[0];      // take the first word
+            String[] slash = curr.split("/");
 
             if (Objects.equals(curr, "bye")) {
                 running = false;
+            } else if (Objects.equals(words[0], "todo") ||
+                       Objects.equals(words[0], "event") ||
+                        Objects.equals(words[0], "deadline")) {
+                String str = words[0];
+                if (Objects.equals(str, "todo")) {
+                    Task newTask = new ToDo(slash[0].substring(5));
+                    list[index] = newTask;
+                    index++;
+
+                    System.out.printf("Steady! I add this already:\n  " + newTask + "\n");
+                    System.out.printf("Now your list got %d tasks.\n", index);
+                } else if (Objects.equals(str, "event")) {
+                    Task newTask = new Event(slash[0].substring(6),slash[1], slash[2]);
+                    list[index] = newTask;
+                    index++;
+
+                    System.out.printf("Steady! I add this already:\n  " + newTask + "\n");
+                    System.out.printf("Now your list got %d tasks.\n", index);
+                } else {
+                    Task newTask = new Deadline(slash[0].substring(9), slash[1]);
+                    list[index] = newTask;
+                    index++;
+
+                    System.out.printf("Steady! I add this already:\n  " + newTask + "\n");
+                    System.out.printf("Now your list got %d tasks.\n", index);
+                }
             } else if (Objects.equals(firstWord, "mark")) {
                 int x = Integer.valueOf(words[1]);
                 Task currTask = list[x - 1];
