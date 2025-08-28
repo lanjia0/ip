18257c775ef6f7b7d6a96ddc4757704a28ffcca6
lanjia0 <nicholas.chin.wai.kit@gmail.com>
2025-08-28 17:16:29 +0800

    implement echo feature

--- src/main/java/AngSoonTong.java
+++ src/main/java/AngSoonTong.java
@@ -1,6 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class AngSoonTong {
     public static void main(String[] args) {
         System.out.println("Eh! I'm Soon Tong\nWhat you want?!");
-        System.out.println("Bye. Why you still here?!");
+
+        boolean running = true;
+        Scanner sc = new Scanner(System.in); // initialise scanner
+
+        while (running) {
+            String curr = sc.nextLine(); // to get user input
+
+            if (Objects.equals(curr, "bye")) { // if user inputs bye, end while loop
+                running = false;
+            } else {
+                System.out.printf("%s", curr); // echoes the user's input
+            }
+        }
+
+        System.out.println("Bye. Why you still here?!"); // ending message
     }
 }
