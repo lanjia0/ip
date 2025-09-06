6e6f705ddaabde7aa063d16c31d253ae164f6bd2
lanjia0 <nicholas.chin.wai.kit@gmail.com>
2025-09-06 23:29:55 +0800

    implement test structure and ParserTest class for JUnit testing

---
+++ src/test/java/angsoontong/parser/ParserTest.java
@@ -0,0 +1,38 @@
+package angsoontong.parser;
+
+import angsoontong.task.*;
+import angsoontong.ui.Ui;
+import angsoontong.storage.Storage;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    public void parse_addTodo_success() {
+        Storage storage = new Storage("data/test-data.txt");
+        TaskList tasks = new TaskList();
+        Ui ui = new Ui();
+
+        String response = Parser.parse("todo read book", tasks, ui, storage);
+
+        assertEquals(1, tasks.size());
+        assertTrue(tasks.get(0) instanceof ToDo);
+        assertTrue(response.contains("Steady! I add this already"));
+    }
+
+    @Test
+    public void parse_markTodo_success() {
+        Storage storage = new Storage("data/test-data.txt");
+        TaskList tasks = new TaskList();
+        Ui ui = new Ui();
+
+        tasks.add(new ToDo("write tests"));
+        Parser.parse("mark 1", tasks, ui, storage);
+
+        assertTrue(tasks.get(0).isDone());
+    }
+}
---
+++ src/test/java/angsoontong/task/TaskListTest.java
@@ -0,0 +1,11 @@
+package angsoontong.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskListTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
